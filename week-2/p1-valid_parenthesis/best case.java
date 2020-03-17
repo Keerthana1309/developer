f4b98c6a129458725e537706c7d5959b28b1ef9e
Keerthana1309 <53963507+Keerthana1309@users.noreply.github.com>
2020-03-17 23:07:33 +0530

    added p1-valid_parenthesis into week-2

---
+++ week-2/p1-valid_parenthesis/best case.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class valid_parenthesis
+{
+       static int count=0;
+       public static void main(String args[])
+      {
+           Scanner sc=new Scanner(System.in);
+           int N=sc.nextInt();
+           int count=0;
+           System.out.println(ValidParenthesis(N,N));
+      }
+      static int ValidParenthesis(int open,int close)
+      {
+            if(open==0&&close==0)
+               count++;
+            if(open>close)
+                return count;
+            if(open>0)
+                ValidParenthesis(open-1,close);
+            if(close>0)
+                ValidParenthesis(open,close-1);
+            return count;
+      }
+}
+           
+     
